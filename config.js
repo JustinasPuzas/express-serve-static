589498e72c3586a6586bc96ab0725a7d93318ed0
Justinas <68568214+JustinasPuzas@users.noreply.github.com>
2022-07-12 13:42:30 +0300

    added config file
    added config file to change react app name and port

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    PORT: 5001,
+    PATH: "my-app" // path to react build folder (relative to server.js) example my-app is in same directory as server.js and build is in my-app
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const config = require("./config");
 
-app.use(express.static(path.join(__dirname, ".." , "my-app/build")));
-console.log(path.join(__dirname, ".." , "my-app/build"));
+app.use(express.static(path.join(__dirname, "..", `${config.PATH}`, "build")));
+console.log(path.join(__dirname, "..", `${config.PATH}`, "/build"));
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "my-app/build", "index.html"));
-  });
+app.use(async (req, res, next) => {
+  res.sendFile(
+    path.join(__dirname, "..", `${config.PATH}`, "build", "index.html")
+  );
+});
 
-app.listen(5000, () => {
-    console.log("server started on port 5000");
-  });
+app.listen(config.PORT, () => {
+  console.log(`server started on port ${config.PORT}`);
+});
